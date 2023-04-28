7b7da6b30a92234839a97a50866a73299581c6df
AndriusG <agedvilasplay@gmail.com>
2023-04-28 19:56:06 +0300

    pagaliau

--- projektas_saldytuvas.py
+++ projektas_saldytuvas.py
@@ -4,6 +4,17 @@ os.system("cls")
 
 produktai_saldytuve = {}
 
+# atidarykite failą ir perskaitykite jo turinį
+try:
+    with open('saldytuvo_save.txt', 'r') as f:
+        for line in f:
+            # atskirkite pavadinimą ir kiekį naudojant split() funkciją
+            produktas, kiekis = line.strip().split()
+            # pridėkite informaciją į žodyną
+            produktai_saldytuve[produktas] = float(kiekis)
+except FileNotFoundError:
+    print("Klaida: nurodytas failas nebuvo rastas.")
+
 while True:
     os.system("cls") 
 
@@ -23,61 +34,53 @@ while True:
     if m_pasirinkimas == "1":
         os.system("cls")
 
-        print("\x1b[36m" + "Produktų sąrašas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
-        print("\x1b[32m" + "Įdėti" + "\x1b[0m" + "\n")
+        print("\x1b[36m" + "Produktu sarasas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
+        print("\x1b[32m" + "Ideti" + "\x1b[0m" + "\n")
 
-        produktas = input("Produktas: ")
-        kiekis_str = input("Kiekis: ")
-        kiekis = float(kiekis_str[:-2])
+    
+        produkto_pavadinimas = input("Iveskite produkto pavadinima: ")
+        kiekis = float(input("Iveskite produkto kieki: "))
 
-        if kiekis_str[-2:] == "kg":
-            kiekis = str(kiekis) + "kg"
+        if produkto_pavadinimas in produktai_saldytuve:
+            produktai_saldytuve[produkto_pavadinimas] += kiekis
         else:
-            kiekis = str(kiekis)
-
-        if produktas in produktai_saldytuve:
-           produktai_saldytuve[produktas] += kiekis
-        else:
-           produktai_saldytuve[produktas] = kiekis
+            produktai_saldytuve[produkto_pavadinimas] = kiekis
 
     # Meniu_pasirinkimas - 2
 
     if m_pasirinkimas == "2":
         os.system("cls")
 
-        print("\x1b[36m" + "Produktų sąrašas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
-        print("\x1b[32m" + "Įdėti" + "\x1b[0m" + "\n")
+        print("\x1b[36m" + "Produktu sarasas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
+        print("\x1b[35m" + "Isimti" + "\x1b[0m" + "\n")
 
-        produktas = input("Produktas: ")
-        kiekis_str = input("Kiekis: ")
-        kiekis = float(kiekis_str[:-2])
+        produktas_pavadinimas = input("Iveskite produkto pavadinima: ")
+        kiekis = float(input("Iveskite produkto kieki: "))
 
-        if kiekis_str[-2:] == "kg":
-            kiekis = str(kiekis) + "kg"
+        if produktas_pavadinimas in produktai_saldytuve:
+           produktai_saldytuve[produktas_pavadinimas] -= kiekis
+           if produktai_saldytuve[produktas_pavadinimas] <= 0:
+              del produktai_saldytuve[produktas_pavadinimas]
         else:
-            kiekis = str(kiekis)
-
-        if produktas in produktai_saldytuve:
-           produktai_saldytuve[produktas] -= kiekis
-        else:
-           produktai_saldytuve[produktas] = kiekis
-        if produktas in produktai_saldytuve:
-           produktai_saldytuve[produktas] -= kiekis
-           if produktai_saldytuve[produktas] <= 0:
-                del produktai_saldytuve[produktas]
+           produktai_saldytuve[produktas_pavadinimas] = kiekis
 
     # Meniu_pasirinkimas - 3 
-    
+
     elif m_pasirinkimas == "3":
         os.system("cls")
         print(f"\x1b[36m" + "Produktu sarasas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
         input("Spauskite " + "\x1b[35m" + "ENTER" + "\x1b[0m" + " , kad grizti i " 
-              + "\x1b[32m" + "MENIU " + "\x1b[0m")
+            + "\x1b[32m" + "MENIU " + "\x1b[0m")
 
     # Meniu_pasirinkimas - 0 
     elif m_pasirinkimas == "0":
         os.system("cls")
         print("-------" + "\x1b[33m" + " Gražios dienos! " + "\x1b[0m" + "-------")
         break
-    
-    ## Nauja uzduotis: iterpti kilogramus, litrus, vienetus.
+
+    # įrašykite žodyną atgal į failą
+with open('saldytuvo_save.txt', 'w') as f:
+    for produktas_pavadinimas, kiekis in produktai_saldytuve.items():
+        f.write(f"{produktas_pavadinimas} {kiekis}\n")
+# Uždaryti failą
+f.close()
