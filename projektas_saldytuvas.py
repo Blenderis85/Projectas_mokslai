b81570c564156f945b62f36fc67048327d285989
AndriusG <agedvilasplay@gmail.com>
2023-04-28 20:09:00 +0300

    atnaujinti

--- projektas_saldytuvas.py
+++ projektas_saldytuvas.py
@@ -4,13 +4,10 @@ os.system("cls")
 
 produktai_saldytuve = {}
 
-# atidarykite failą ir perskaitykite jo turinį
 try:
     with open('saldytuvo_save.txt', 'r') as f:
         for line in f:
-            # atskirkite pavadinimą ir kiekį naudojant split() funkciją
             produktas, kiekis = line.strip().split()
-            # pridėkite informaciją į žodyną
             produktai_saldytuve[produktas] = float(kiekis)
 except FileNotFoundError:
     print("Klaida: nurodytas failas nebuvo rastas.")
@@ -29,15 +26,12 @@ while True:
                            + " (" + "\x1b[31m" + "0" + "\x1b[0m" + "-" + "\x1b[34m" 
                            + "3" + "\x1b[0m" + "): ")
 
-    # Meniu pasirinkimas - 1
-
     if m_pasirinkimas == "1":
         os.system("cls")
 
         print("\x1b[36m" + "Produktu sarasas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
         print("\x1b[32m" + "Ideti" + "\x1b[0m" + "\n")
 
-    
         produkto_pavadinimas = input("Iveskite produkto pavadinima: ")
         kiekis = float(input("Iveskite produkto kieki: "))
 
@@ -45,8 +39,6 @@ while True:
             produktai_saldytuve[produkto_pavadinimas] += kiekis
         else:
             produktai_saldytuve[produkto_pavadinimas] = kiekis
-
-    # Meniu_pasirinkimas - 2
 
     if m_pasirinkimas == "2":
         os.system("cls")
@@ -62,25 +54,20 @@ while True:
            if produktai_saldytuve[produktas_pavadinimas] <= 0:
               del produktai_saldytuve[produktas_pavadinimas]
         else:
-           produktai_saldytuve[produktas_pavadinimas] = kiekis
-
-    # Meniu_pasirinkimas - 3 
+           produktai_saldytuve[produktas_pavadinimas] = kiekis 
 
     elif m_pasirinkimas == "3":
         os.system("cls")
         print(f"\x1b[36m" + "Produktu sarasas: " + "\x1b[0m" + str(produktai_saldytuve) + "\n")
         input("Spauskite " + "\x1b[35m" + "ENTER" + "\x1b[0m" + " , kad grizti i " 
             + "\x1b[32m" + "MENIU " + "\x1b[0m")
-
-    # Meniu_pasirinkimas - 0 
+ 
     elif m_pasirinkimas == "0":
         os.system("cls")
         print("-------" + "\x1b[33m" + " Gražios dienos! " + "\x1b[0m" + "-------")
         break
 
-    # įrašykite žodyną atgal į failą
 with open('saldytuvo_save.txt', 'w') as f:
     for produktas_pavadinimas, kiekis in produktai_saldytuve.items():
         f.write(f"{produktas_pavadinimas} {kiekis}\n")
-# Uždaryti failą
 f.close()
