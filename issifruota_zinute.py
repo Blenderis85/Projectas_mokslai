e78ce22481a3ab9987e181153f944d49fa244567
AndriusG <agedvilasplay@gmail.com>
2023-05-11 13:34:42 +0300

    updeitas

---
+++ issifruota_zinute.py
@@ -0,0 +1,14 @@
+def decrypt_char(char, key):
+    return chr(((ord(char) - ord('A')) + 26 - key) % 26 + ord('A'))
+
+def decrypt_message(cipher, key):
+    message = ''
+    for c in cipher:
+        if c in ' ,.':
+            message = message + c
+        else:
+            message = message + decrypt_char(c, key)
+    return message
+
+decrypted_message = decrypt_message('BRX DUH DZHVRPH', 3)
+print(decrypted_message)
